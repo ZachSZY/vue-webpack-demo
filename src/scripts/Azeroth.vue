e7773e3b4336508582cbe3d2b3bbcfafdf9f3ce1
JackyShen <mebusw@163.com>
2016-08-04 17:57:08 +0800

    demo of bi-direction binding of props passing value between parent and children components.

---
+++ src/scripts/Azeroth.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>In {{locname}}, you have ${{money}}</h1>
+    <button id="incMoney" @click='incMoney'>incMoney</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    money: {
+      type: Number
+    }
+  },
+  data () {
+    return {
+      mm: this.money,
+      locname: 'Azeroth'
+    }
+  },
+  methods: {
+    incMoney: function (amount) {
+      console.log('incMoney...ing ' + this.money)
+      this.money += 10
+      console.log('incMoney...ed ' + this.money)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #00FF00;
+}
+</style>
---
+++ src/scripts/Draenor.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>In {{locname}}, you have ${{money}}</h1>  
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    money: {
+      type: Number
+    }
+  },
+  data () {
+    return {
+      locname: 'Draenor'
+    }
+  },
+  methods: {
+    incMoney: function (amount) {
+      this.money += amount
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #0000FF;
+}
+</style>
