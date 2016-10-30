2522188b4100559d1c60024ed4ff5d7b84fbeecf
JackyShen <mebusw@163.com>
2016-10-30 21:09:50 +0800

    timely refresh for lastest git commit info and travis-ci status

--- src/scripts/TravisCI.vue
+++ src/scripts/TravisCI.vue
@@ -1,9 +1,15 @@
 <template>
     <h1>Travis-CI for CSD</h1>
 
-    <h2 id="datetime">{{now}}</h2>
+    <h2 id="datetime">{{now | formatDate}}</h2>
     <ul v-for="branch in branches">
-        <li><b>{{branch}} </b><img src="https://travis-ci.org/mebusw/CSD-training-prj.svg?branch={{branch}}" alt="build:passed">
+        <li>
+          <p><b>{{branch}} </b><img src="https://travis-ci.org/mebusw/CSD-training-prj.svg?branch={{branch}}" alt="build:passed"></p>
+          <p><a :href="commits[branch].html_url" target="_blank" class="commit">{{commits[branch].sha.slice(0, 7)}}</a>
+          - <span class="message">{{commits[branch].message | truncate}}</span><br>
+          by <span class="author">{{commits[branch].author.name}}</span>
+          at <span class="date">{{commits[branch].author.date | formatDate}}</span>
+          </p>
         </li>
     </ul>
 
@@ -16,33 +22,61 @@ $('')
 export default {
   data () {
     return {
-      msg: 'Hello Vue!',
-      newTodo: '',
-      todos: [
-        { text: 'Learn JavaScript' },
-        { text: 'Learn Vue.js' },
-        { text: 'Build Something Awesome' }
-      ],
       branches: ['master', 'csharp-oct', 'csharp-jpx', 'java-monkey', 'java-eas'],
-      now: 'time: ',
-      currentBranch: 'master',
-      commits: null
+      now: '',
+      commits: {master: {sha: '1234567890', message: 'xxxxxxxxxx', author: {name: 'tom', date: ''}}}
     }
   },
 
   created: function () {
-    this.now = 'time: ' + new Date()
-    setTimeout(function () {
-      window.location.reload()
+    var self = this
+    self.fetchData()
+    setInterval(function () {
+      self.fetchData()
     }, 15000)
   },
 
-  methods: {
-    addTodo: function () {
-      this.todos.push({text: this.newTodo})
+  filters: {
+    truncate: function (v) {
+      if (v === undefined) return ''
+      var newline = v.indexOf('\n')
+      return newline > 0 ? v.slice(0, newline) : v
     },
-    removeTodo: function (index) {
-      this.todos.splice(index, 1)
+    formatDate: function (v) {
+      if (v === undefined) return ''
+      return v.replace(/T|Z/g, ' ')
+    }
+  },
+
+  methods: {
+    // addTodo: function () {
+    //   this.todos.push({text: this.newTodo})
+    // },
+    // removeTodo: function (index) {
+    //   this.todos.splice(index, 1)
+    // },
+    fetchData: function () {
+      var apiURL = 'https://api.github.com/repos/mebusw/CSD-training-prj/commits?per_page=1&sha='
+      var self = this
+      this.now = 'Last refreshed at: ' + new Date()
+
+      self.branches.forEach(function (branch, i) {
+        $.ajax({
+          method: 'GET',
+          url: apiURL + branch,
+          data: {}
+        })
+        .done(function (data) {
+          console.log('done')
+          self.commits[branch] = data
+        })
+        .fail(function () {
+          console.log('error')
+        })
+        .always(function () {
+          console.log('complete')
+        })
+      })
     }
   }
 }
@@ -66,6 +100,6 @@ li {
 }
 
 ul {
-    font-size: 36px;
+    font-size: 24px;
 }
 </style>
